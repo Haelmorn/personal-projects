943694ce9b13d49dbd0aee7cae947e96a0f5c1d6
haelmorn <haelmorn@gmail.com>
2019-01-06 02:42:28 +0100

    Replacing test message from GCpercentage.py

--- GCpercentage.py
+++ GCpercentage.py
@@ -22,5 +22,4 @@ for k, v in textd.items():
 #find and print the highest GC percentage alongside with its key
 maximum = max(a, key=a.get)
 print(maximum,"\n",a[maximum]*100)
-
-TEST MESSAGE FOR GIT PULL
+REPLACING GIT TEST MESSAGE WITH ANOTHER TEST MESSAGE
