c993aaf4629ba69b20b9f21055d0deca16d929ee
haelmorn <haelmorn@gmail.com>
2019-01-06 02:33:58 +0100

    Update some files

--- fibbonacirabbits.py
+++ fibbonacirabbits.py
@@ -1,12 +1,6 @@
-def fibbonaci(n):
-    if n < 2:
-        return n
-    return fibbonaci(n-2) + fibbonaci(n-1)
-n = 40
-m = 16
-all = 0
-for i in range(0, n-m+1):
-    all += fibbonaci(i)
-
-sol = fibbonaci(n) - all
-print(sol)
+n = 88
+m = 20
+age = [1]+ [0] * (m-1)
+for i in range(0, n-1):
+    age = [sum(age[1:])]+age[:-1]
+print(sum(age))
