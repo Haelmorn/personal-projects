762b3cfbd2f50fd137588e6141ac3b004cb4ef71
haelmorn <haelmorn@gmail.com>
2019-01-27 14:30:32 +0100

    Fixed the script to return proper restriction sites

--- restrictionsites.py
+++ restrictionsites.py
@@ -1,17 +1,15 @@
-from Bio.Seq import Seq
-from Bio.Alphabet import generic_dna
 from readros import read_rosalind as rr 
+from reversecomplement import reverse_complement
 
 sequence = rr("test.txt")
 sequence = list(sequence.values())
 sequence = str(sequence[0])
-print(sequence)
-sequence = Seq(sequence, alphabet=generic_dna)
-rev = sequence.reverse_complement()
-print(rev)
-c = []
-for i in range(2, len(rev)):
-    for j in range(0, len(rev)):
-        if i + j <= len(rev):
-            if sub[j:j+i] == rev[j:j+i]
-                    c.append(len(sub))
+diki = []
+for i in range(12, 3, -2):
+        for j in range(0, len(sequence)-i+1):
+                seq = sequence[j:j+i]
+                if seq == reverse_complement(seq):
+                        diki.append([j+1, i])
+
+for pair in diki:
+        print(pair[0], "\t", pair[1])
