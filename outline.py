0ddb6c856cb3f881c4c280cc8787c038c558618e
haelmorn <haelmorn@gmail.com>
2019-02-22 10:00:49 +0100

    refactor of logging function

--- outline.py
+++ outline.py
@@ -1,10 +1,14 @@
+# -*- coding: UTF-8 -*-
 import requests
 import smtplib, ssl
 import os
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import datetime
 
-websites = ["https://www.google.com/", "https://medium.freecodecamp.org/"]
+websites = ["https://www.google.com/", "https://httpstat.us/502"]
+
 
 def main():
     context = ssl.create_default_context()
@@ -16,8 +20,9 @@ def main():
 
         for website in websites:
             status = status_check(website)
-            msg = compose_message(website, status)
+            msg = compose_email(website, status)
             server.send_message(msg)
+
 
 def status_check(address):
     # Takes website address as an argument and uses requests lib to check if status code is equal to 200
@@ -27,26 +32,36 @@ def status_check(address):
             return True
         else:
             return False
-    except Exception as e:
-        write_log(e)
+    except Exception:
+        return False
 
-def compose_email(website, status):
+
+def compose_email(website, status, error_message=''):
     # builds message based on server response - status
 
     msg = MIMEMultipart()
     msg["From"] = "ciuchcia98@gmail.com"
     msg["To"] = "haelmorn@gmail.com"
-    msg["Subject"] = f"{page} is {text}"
+    if status == True:
+        msg["Subject"] = u"\u2713 STATUS OK: {}".format(website)
+        write_log(f"STATUS OK: {website}")
+    else:
+        try:
+            code = requests.get(f"{website}").status_code
+        except Exception as e:
+            code = e
+        msg["Subject"] = u"\u274C ERROR {}: {}".format(code,website)
+        write_log(f"ERROR {code}: {website}")
 
     return msg
 
 
-def write_log(error_message):
+def write_log(log_message):
     with open("logfile.txt", "a+") as log_file:
         now = datetime.datetime.now()
         log_file.write(str(now))
-        log_file.write("\n")
-        log_file.write(str(error_message))
+        log_file.write(" - ")
+        log_file.write(str(log_message))
         log_file.write("\n")
 
 
