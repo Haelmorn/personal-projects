660c333fa7fb995758a2b7b6894415d4c9704a2a
haelmorn <haelmorn@gmail.com>
2019-05-03 14:37:31 +0200

    Fixed rear.py

--- rear.py
+++ rear.py
@@ -1,45 +1,52 @@
 import random
 from itertools import combinations
-import copy
+from profilestats import profile
 
-#with open('test.txt', 'r') as file:
-#        temp = file.read().splitlines()
-#        temp = [line.split() for line in temp if line != ""]
-
-A = [[1,2,3,4,5,6,7,8,9,10], [3,1,5,2,7,4,9,6,10,8]]
-
-B = [[3,10,8,2,5,4,7,1,6,9], [5,2,3,1,7,4,10,8,6,9]]
+with open('rosalind_rear.txt', 'r') as file:
+    temp = file.read().splitlines()
+    temp = [line.split() for line in temp if line != ""]
 
 
 def rev(li, i1, i2):
     li[i1:i2+1] = li[i1:i2+1][::-1]
     return li
 
+@profile(print_stats=20)
 def similarity(li, goal_comb):
-    similarity = 0
-    for i in range(len(li)):
-        if li[i] == goal_comb[i]:
-            similarity += 1
-    return similarity/len(li)
+    #similarity = 0
+    #for i in range(len(li)):
+        #if li[i] == goal_comb[i]:
+            #similarity += 1
+    #return similarity
+    return len([i for i, j in zip(li, goal_comb) if i == j])
 
 def perms(list2, goal_comb):
     diki = {}
     for i, j in combinations(range(0, 11), 2):
-        target = copy.deepcopy(list2)
+        target = list2[:]
         current_perm = rev(target, i, j)
         current_similarity = similarity(current_perm, goal_comb)
         diki[tuple(current_perm)] = current_similarity
     maxValue = max(diki.values())
-    maxes = [k for k, v in diki.items() if v == maxValue]
+    maxes = [k for k, v in diki.items() if v >= maxValue*0.5]
 
     return [list(i) for i in maxes]
 
 
 def run():
-    reversals = 1
-    goal = A[1]
-    permuts = perms(A[0], goal)
-    while goal not in permuts:
-        permuts = [perms(i, goal) for i in permuts]
-        reversals += 1
-    print(reversals)
+    sets = ((temp[0], temp[1]), (temp[2], temp[3]), (temp[4], temp[5]), (temp[6], temp[7]), (temp[8], temp[9]))
+    for pair in sets:
+        if pair[0] == pair[1]:
+            print(0)
+        else:
+            reversals = 1
+            goal = pair[1]
+            permuts = perms(pair[0], goal)
+            while goal not in permuts:
+                permuts = [perms(i, goal) for i in permuts]
+                permuts = sum(permuts, [])
+                permuts = [item for item in permuts if item[0:reversals] == goal[0:reversals] or item[-reversals:] == goal[-reversals:]]
+                reversals += 1
+            print(reversals)
+
+run()
